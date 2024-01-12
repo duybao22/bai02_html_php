8bc16308ddf3e395f49c1b9d9bc0f5d238dba266
duybao22 <149235175+duybao22@users.noreply.github.com>
2024-01-12 16:54:23 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <!-- header -->
+   <?php include_once './inc/header.php' ; ?>
+   <?php include_once './inc/header.php' ; ?>
+    <h1>HTML VA PHP</h1>
+    <?php echo 'Hello World'; ?>
+    <?php
+    $language = 'Ngon Ngu lap Trinh PHP';
+    var_dump($language); ?>
+    <!-- content -->
+     <?php include './inc/content.php'; ?>
+     <?php include './inc/content.php'; ?>
+    <!-- footer -->
+     <?php include_once './inc/footer.php'; ?>
+     <?php include_once './inc/footer.php'; ?>
+
+</body>
+</html>
